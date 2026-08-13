dd3cf9b0fdd2be4248b0cbc10117bd88768d4e10
lostlove2025ar-sudo <lostlove2025ar@gmail.com>
2026-08-13 08:32:35 +0600

    Rename NetflixApp to MoviesApp and update UI
    Refactored NetflixApp to MoviesApp and updated UI elements. Added a list of movies with titles and banners.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 
 void main() {
-  runApp(const NetflixApp());
+  runApp(const MoviesApp());
 }
 
-class NetflixApp extends StatelessWidget {
-  const NetflixApp({super.key});
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF141414),
-        primaryColor: const Color(0xFFE50914),
+        scaffoldBackgroundColor: Colors.black,
       ),
       home: const HomeScreen(),
     );
@@ -24,146 +22,135 @@ class NetflixApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  final List<Map<String, String>> movies = const [
+    {
+      'title': 'Oru Durooha Saahacharyathil',
+      'banner': 'https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=800',
+      'category': 'Featured'
+    },
+    {
+      'title': 'Stranger Things',
+      'banner': 'https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?w=500',
+      'category': 'Trending'
+    },
+    {
+      'title': 'The Dark Knight',
+      'banner': 'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?w=500',
+      'category': 'Trending'
+    },
+    {
+      'title': 'Inception',
+      'banner': 'https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?w=500',
+      'category': 'Popular'
+    },
+    {
+      'title': 'Interstellar',
+      'banner': 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?w=500',
+      'category': 'Popular'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: const Text(
-          'NETFLIX',
-          style: TextStyle(
-            color: Color(0xFFE50914),
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-            letterSpacing: -1,
+        backgroundColor: Colors.black,
+        title: const Text('NETFLIX', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 24)),
+        actions: [
+          IconButton(icon: const Icon(Icons.search), onPressed: () {}),
+          const CircleAvatar(
+            backgroundColor: Colors.red,
+            child: Text('MB', style: TextStyle(color: Colors.white, fontSize: 12)),
           ),
-        ),
-        actions: const [
-          Icon(Icons.search, size: 28),
-          SizedBox(width: 15),
-          CircleAvatar(
-            backgroundColor: Color(0xFFE50914),
-            radius: 14,
-            child: Text('MB', style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold)),
-          ),
-          SizedBox(width: 15),
+          const SizedBox(width: 12),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              height: 350,
-              width: double.infinity,
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: const DecorationImage(
-                  image: NetworkImage('https://image.tmdb.org/t/p/w500/d5N2232T32vi3Sc234L0zz.jpg'),
+            // Featured Banner
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Image.network(
+                  movies[0]['banner']!,
+                  height: 350,
+                  width: double.infinity,
                   fit: BoxFit.cover,
                 ),
-              ),
-              child: Container(
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  gradient: const LinearGradient(
-                    colors: [Colors.black, Colors.transparent, Colors.black87],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
+                Container(
+                  height: 350,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.transparent, Colors.black.withOpacity(0.9)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
                   ),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Oru Durooha Saahacharyathil',
-                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white),
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
+                Positioned(
+                  bottom: 20,
+                  child: Column(
+                    children: [
+                      Text(movies[0]['title']!, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
+                      const SizedBox(height: 10),
+                      ElevatedButton.icon(
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.white, foregroundColor: Colors.black),
+                        onPressed: () {},
+                        icon: const Icon(Icons.play_arrow),
+                        label: const Text('Play'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            
+            // Catalog Section
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text('Movies Bazar Catalog', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)),
+            ),
+            const SizedBox(height: 12),
+            
+            SizedBox(
+              height: 180,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: movies.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(left: 16),
+                    width: 120,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Expanded(
-                          child: ElevatedButton.icon(
-                            style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (_) => const PlayerScreen(
-                                    title: 'Oru Durooha Saahacharyathil',
-                                    streamUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-                                  ),
-                                ),
-                              );
-                            },
-                            icon: const Icon(Icons.play_arrow, color: Colors.black),
-                            label: const Text('Play', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            movies[index]['banner']!,
+                            height: 140,
+                            width: 120,
+                            fit: BoxFit.cover,
                           ),
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          movies[index]['title']!,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(color: Colors.white70, fontSize: 12),
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text('Movies Bazar Catalog', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class PlayerScreen extends StatefulWidget {
-  final String title;
-  final String streamUrl;
-  const PlayerScreen({super.key, required this.title, required this.streamUrl});
-
-  @override
-  State<PlayerScreen> createState() => _PlayerScreenState();
-}
-
-class _PlayerScreenState extends State<PlayerScreen> {
-  late VideoPlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.streamUrl))
-      ..initialize().then((_) {
-        setState(() {});
-        _controller.play();
-      });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.black,
-      ),
-      body: Center(
-        child: _controller.value.isInitialized
-            ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              )
-            : const CircularProgressIndicator(color: Color(0xFFE50914)),
       ),
     );
   }
